ff001c2303fcdea8e1cc0c6a00ae46b43dccc266
Igor Pereira Lobo <igorplobo@gmail.com>
2024-06-21 11:43:07 -0300

    Tela 1
    Nova tela do menu

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -75,6 +75,7 @@ export const appRoutes: Route[] = [
         },
         children: [
             {path: 'example', loadChildren: () => import('app/modules/admin/example/example.routes')},
+            {path: 'formulario', loadChildren: () => import('app/modules/admin/formulario/formulario.routes')},
         ]
     }
 ];
---
+++ src/app/modules/admin/formulario/formulario.routes.ts
@@ -0,0 +1,9 @@
+import { Routes } from '@angular/router';
+import { FormularioComponent } from './formulario.component';
+
+export default [
+    {
+        path     : '',
+        component: FormularioComponent,
+    },
+] as Routes;
